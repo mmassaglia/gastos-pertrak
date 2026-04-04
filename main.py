7ce408cc2154aa6301ece35930327b49fccc44e5
mmassaglia <56165831+mmassaglia@users.noreply.github.com>
2026-04-04 14:06:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-rom fastapi import FastAPI, HTTPException, Query
+from fastapi import FastAPI, HTTPException, Query
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Optional
